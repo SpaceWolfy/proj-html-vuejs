7cbff0a1869f9a04720ab5f9b4963d2401bf58fe
Samuel Scozzari <s_scozzari@libero.it>
2022-02-08 13:11:23 +0100

    Header section generally completed

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -2,18 +2,22 @@
   <header>
     <div class="logo"><img src="../assets/img/logo-2.png" alt="" /></div>
 
-    <nav>
-      <ul v-for="items in headerNav" :key="items">
-        <li>{{ items }}</li>
-      </ul>
-    </nav>
+    <div class="right-side">
+      <nav>
+        <ul v-for="items in headerNav" :key="items">
+          <li>
+            <a href="">{{ items }}</a>
+          </li>
+        </ul>
+      </nav>
 
-    <div class="iconFunctions">
-      <i class="fas fa-search"></i>
-      <i class="fas fa-shopping-cart"></i>
+      <div class="icon-functions">
+        <i class="fas fa-search"></i>
+        <i class="fas fa-shopping-cart"></i>
+      </div>
+
+      <button class="general-btn">Get Started</button>
     </div>
-
-    <button class="general-btn">Get Started</button>
   </header>
 </template>
 
@@ -27,4 +31,64 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/style/Variables.scss";
+@import "@/style/Utility.scss";
+
+header {
+  height: 70px;
+  padding-top: 12px;
+  display: flex;
+  justify-content: space-around;
+
+  .logo {
+    width: 45px;
+    margin-right: 100px;
+    img {
+      width: 100%;
+    }
+  }
+
+  .right-side {
+    display: flex;
+    align-items: center;
+
+    nav {
+      display: flex;
+      padding-bottom: 6px;
+
+      ul {
+        list-style: none;
+        font-size: 17px;
+        padding: 17px;
+
+        li {
+          padding: 17px 0;
+          border-bottom: 2px solid transparent;
+          &:hover {
+            border-bottom: 2px solid $blue;
+          }
+
+          a {
+            color: $darkGrey;
+          }
+
+          &:hover a {
+            color: $blue;
+          }
+        }
+      }
+    }
+
+    .icon-functions {
+      margin: 35px;
+
+      .fas {
+        margin: 15px;
+
+        &:hover {
+          color: $blue;
+        }
+      }
+    }
+  }
+}
 </style>
