f10df107ede9a87ec3590d0a5db70f57894efcc8
Samuel Scozzari <s_scozzari@libero.it>
2022-02-08 12:26:31 +0100

    added general elements

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,7 +1,30 @@
-<template><header></header></template>
+<template>
+  <header>
+    <div class="logo"><img src="../assets/img/logo-2.png" alt="" /></div>
+
+    <nav>
+      <ul v-for="items in headerNav" :key="items">
+        <li>{{ items }}</li>
+      </ul>
+    </nav>
+
+    <div class="iconFunctions">
+      <i class="fas fa-search"></i>
+      <i class="fas fa-shopping-cart"></i>
+    </div>
+
+    <button class="general-btn">Get Started</button>
+  </header>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    headerNav: Array,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/style/Variables.scss";
+</style>
