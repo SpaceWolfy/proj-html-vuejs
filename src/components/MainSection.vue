f66e4a1db8687efb32fe6b091460b43f2acd7e2e
Samuel Scozzari <s_scozzari@libero.it>
2022-02-08 11:56:37 +0100

    connection with secondary components

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,7 +1,41 @@
-<template><main></main></template>
+<template>
+  <main>
+    <jumbo-section />
+
+    <online-marketing />
+
+    <tip-section />
+
+    <feature-section />
+
+    <testimonials />
+
+    <posts-section />
+
+    <contact-section />
+  </main>
+</template>
 
 <script>
-export default {};
+import ContactSection from "./MainSection subsections/ContactSection.vue";
+import FeatureSection from "./MainSection subsections/FeatureSection.vue";
+import JumboSection from "./MainSection subsections/JumboSection.vue";
+import OnlineMarketing from "./MainSection subsections/OnlineMarketing.vue";
+import PostsSection from "./MainSection subsections/PostsSection.vue";
+import Testimonials from "./MainSection subsections/Testimonials.vue";
+import TipSection from "./MainSection subsections/TipSection.vue";
+
+export default {
+  components: {
+    JumboSection,
+    OnlineMarketing,
+    TipSection,
+    FeatureSection,
+    Testimonials,
+    PostsSection,
+    ContactSection,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
