941e1c7d461395e8cda2cef3a188cb60d3ebbf82
Samuel Scozzari <s_scozzari@libero.it>
2022-02-08 12:26:12 +0100

    added header array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <header-box />
+    <header-box :headerNav="headerNav" />
     <main-section />
     <footer-box />
   </div>
@@ -14,6 +14,12 @@ import FooterBox from "./components/FooterBox.vue";
 export default {
   name: "App",
   components: { HeaderBox, MainSection, FooterBox },
+  data() {
+    return {
+      //array semplice - TODO: trasformazione in array di oggetti con link di collegamento alle sezioni del sito
+      headerNav: ["Home", "About Us", "Feature", "Testimonials", "Contact US"],
+    };
+  },
 };
 </script>
 
