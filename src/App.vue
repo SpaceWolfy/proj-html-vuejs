a5287a2d24351462129020dd3b61ecfcdfa5ab31
Samuel Scozzari <s_scozzari@libero.it>
2022-02-08 14:10:29 +0100

    Added loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <header-box :headerNav="headerNav" />
-    <main-section />
-    <footer-box />
+    <div v-if="!isLoading">
+      <loading-screen />
+    </div>
+    <div v-else>
+      <header-box :headerNav="headerNav" />
+      <main-section />
+      <footer-box />
+    </div>
   </div>
 </template>
 
@@ -10,15 +15,20 @@
 import HeaderBox from "./components/HeaderBox.vue";
 import MainSection from "./components/MainSection.vue";
 import FooterBox from "./components/FooterBox.vue";
+import LoadingScreen from "./components/LoadingScreen.vue";
 
 export default {
   name: "App",
-  components: { HeaderBox, MainSection, FooterBox },
+  components: { HeaderBox, MainSection, FooterBox, LoadingScreen },
   data() {
     return {
       //array semplice - TODO: trasformazione in array di oggetti con link di collegamento alle sezioni del sito
       headerNav: ["Home", "About Us", "Feature", "Testimonials", "Contact US"],
+      isLoading: false,
     };
+  },
+  mounted() {
+    setTimeout(() => (this.isLoading = true), 3000);
   },
 };
 </script>
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="../assets/img/loader-2.gif" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100px;
+  }
+}
+</style>
