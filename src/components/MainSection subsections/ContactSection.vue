e0992cbb329c296b99f82047a0c474df893e899b
Samuel Scozzari <s_scozzari@libero.it>
2022-02-10 00:19:37 +0100

    Contact Section generally completed

--- src/components/MainSection subsections/ContactSection.vue
+++ src/components/MainSection subsections/ContactSection.vue
@@ -9,7 +9,11 @@
       <div class="contact-info">
         <div class="left-side">
           <div class="map">
-            <img src="../../assets/img/StaticMapService.GetMapIma.png" alt="" />
+            <img
+              class="map-img"
+              src="../../assets/img/StaticMapService.GetMapIma.png"
+              alt=""
+            />
 
             <div class="indicazioni">
               <div class="left-part">
@@ -51,17 +55,40 @@
             <div>mail@sofbox.com</div>
           </div>
 
-          <div class="social"></div>
+          <div class="social">
+            <div
+              class="icon-cont"
+              v-for="(social, index) in social"
+              :key="index"
+            >
+              <i :class="social"></i>
+            </div>
+          </div>
 
           <div class="input-section">
-            <div class="subtitle"></div>
+            <div class="subtitle">Get in Touch</div>
 
-            <div class="personal info"></div>
+            <div class="personal-info">
+              <input type="text" placeholder="Your Name" />
+              <input type="text" placeholder="Your Email" />
+            </div>
 
-            <div></div>
+            <div class="subject">
+              <input type="text" placeholder="Your Subject" />
+            </div>
 
-            <div class="user-message"></div>
+            <div class="user-message">
+              <textarea
+                name="message"
+                id="contact-msg"
+                cols="30"
+                rows="10"
+                placeholder="Your Message"
+              ></textarea>
+            </div>
           </div>
+
+          <button class="general-btn">Send Message</button>
         </div>
       </div>
     </div>
@@ -69,156 +96,20 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      social: [
+        "fa-brands fa-facebook-f",
+        "fa-brands fa-twitter",
+        "fa-brands fa-github",
+        "fa-brands fa-instagram",
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import "@/style/Variables.scss";
-
-.main {
-  margin: 110px 135px 0 135px;
-
-  .text {
-    text-align: center;
-    margin-bottom: 110px;
-    .title {
-      font-size: 38px;
-      margin-bottom: 20px;
-    }
-
-    p {
-      font-size: 17px;
-      line-height: 30px;
-    }
-  }
-
-  .contact-info {
-    display: flex;
-    .left-side {
-      .map {
-        position: relative;
-        width: 555px;
-        height: 450px;
-
-        .indicazioni,
-        .zoom,
-        .location,
-        .legal-info {
-          position: absolute;
-        }
-
-        .indicazioni {
-          background-color: white;
-          top: 10px;
-          left: 10px;
-          padding: 10px;
-          display: flex;
-          cursor: pointer;
-
-          .left-part {
-            div:first-child {
-              color: $darkGrey;
-              font-weight: 600;
-              font-size: 14px;
-            }
-
-            div:nth-child(2),
-            div:last-child {
-              font-weight: bold;
-              font-size: 12px;
-            }
-
-            div:nth-child(2) {
-              color: #7e7e7e;
-              margin: 5px 0 22px 0;
-            }
-
-            div:last-child {
-              color: $blue;
-            }
-          }
-
-          .right-part {
-            text-align: center;
-            margin: 0 20px 0 40px;
-            img {
-              width: 21px;
-            }
-
-            div {
-              color: $blue;
-              font-size: 12px;
-            }
-          }
-        }
-
-        .zoom {
-          display: flex;
-          flex-direction: column;
-          background-color: white;
-          color: #666666;
-          right: 10px;
-          bottom: 25px;
-          border: 1px solid #e6e6e6;
-          cursor: pointer;
-
-          .line {
-            border-top: 1px solid #e6e6e6;
-            margin: 0 5px;
-          }
-
-          i {
-            padding: 10px;
-            font-size: 23px;
-          }
-        }
-
-        .location {
-          top: 205px;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-
-        .legal-info {
-          display: flex;
-          color: #262826;
-          font-size: 11px;
-          font-weight: bold;
-          bottom: 0;
-          right: 0;
-
-          div {
-            background-color: rgba($color: #e6f1e6, $alpha: 0.8);
-            padding: 2px 8px;
-            margin-left: 2px;
-            cursor: pointer;
-          }
-        }
-      }
-    }
-  }
-
-  .right-side {
-    margin-left: 35px;
-
-    .logo {
-      width: 130px;
-    }
-
-    .street {
-      font-size: 23px;
-      margin: 60px 0 30px 0;
-    }
-
-    .contacts {
-      color: #8e989f;
-      font-weight: bold;
-      font-size: 17px;
-
-      div {
-        margin-bottom: 10px;
-      }
-    }
-  }
-}
+@import "@/style/MainS-Components/ContactSection.scss";
 </style>
